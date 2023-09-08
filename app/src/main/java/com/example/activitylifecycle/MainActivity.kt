f9b14718f56799a3e3f2d2fc7c76ed6d608c12b1
Sila Elijah <elimaithya21@gmail.com>
2023-09-08 12:11:21 +0300

    onStart() OnStop() OnDestroy()

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.activitylifecycle
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -15,15 +17,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.activitylifecycle.ui.theme.ActivityLifecycleTheme
 
+const val Tag = "MainActivity"
 class MainActivity : ComponentActivity() {
     val create: MutableState<String> = mutableStateOf("")
     val pause: MutableState<String> = mutableStateOf("")
     val resume: MutableState<String> = mutableStateOf("")
     val start: MutableState<String> = mutableStateOf("")
+    val stop: MutableState<String> = mutableStateOf("")
+    val destroy: MutableState<String> = mutableStateOf("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         create.value = "onCreateCalled"
+        Log.d(Tag, "On create called")
         setContent {
             ActivityLifecycleTheme {
                 Column() {
@@ -31,9 +37,10 @@ class MainActivity : ComponentActivity() {
 //                    Text(text = )
                     Text(text = create.value)
                     Text(text = start.value)
-
-                    Text(text = pause.value)
                     Text(text = resume.value)
+                    Text(text = pause.value)
+                    Text(text = stop.value)
+
                 }
             }
 
@@ -43,16 +50,31 @@ class MainActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
         start.value = "Onstart Called"
+        Log.d(Tag, "On start called")
     }
 
     override fun onPause() {
         super.onPause()
         pause.value = "onPauseCalled"
+        Log.d(Tag, "On pause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stop.value = " On stop called"
+        Log.d(Tag, "On stop called")
     }
 
     override fun onResume() {
         super.onResume()
         resume.value = "onResumeCalled"
+        Log.d(Tag, "On resume called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        destroy.value = " On destroy called"
+        Log.d(Tag, "On Destroy callled")
     }
 }
 
